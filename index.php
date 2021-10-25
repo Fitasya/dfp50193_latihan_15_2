4220aca8b83fb02f59887a29c0be3bc52330deda
As Syafaa <purpleeisya@gmail.com>
2021-10-25 16:37:57 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pelbagai Soalan</title>
+</head>
+<body>
+    <form action="proses.php" method="post">
+        Nama
+        <textarea name="nama"></textarea><br>
+        Password
+        <input type="password" name="kata">
+        <button type="submit">GO</button>
+    </form>
+    
+</body>
+</html>
